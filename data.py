27248b3b183d3726b2bc2653af809498b84b9754
albahu <97354572+albahu@users.noreply.github.com>
2023-12-27 14:49:46 -0700

    Add files via upload

---
+++ data.py
@@ -0,0 +1,6 @@
+urban_routes_url = 'https://68411ce0-cfb1-4d43-869e-fc67c868f64c.serverhub.tripleten-services.com?lng=es'
+address_from = 'East 2nd Street, 601'
+address_to = '1300 1st St'
+phone_number = '+1 123 123 12 12'
+card_number, card_code = '1234 5678 9100', '111'
+message_for_driver = 'Muéstrame el camino al museo'
